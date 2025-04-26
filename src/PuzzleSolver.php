0ee7cab8344a3ad9a39e0043815e6fd4a51978c9
mario.dominguez1 <mario.dominguez@fashiola.com>
2025-04-26 13:05:43 +0200

    Refactor add code breakpoint when uzzleSolution generated

--- src/PuzzleSolver.php
+++ src/PuzzleSolver.php
@@ -25,29 +25,29 @@ final readonly class PuzzleSolver
     {
         if (count($puzzlePieces) === 1) {
             $firstPiece = $puzzlePieces[0];
-            $puzzleSolutionIndex = new PuzzleSolution([[$firstPiece->id]]);
+            return new PuzzleSolution([[$firstPiece->id]]);
         }
 
         if (count($puzzlePieces) === 2) {
             $firstPiece = $puzzlePieces[0];
             $secondPiece = $puzzlePieces[1];
             if ($firstPiece->matchVertically($secondPiece)) {
-                $puzzleSolutionIndex = new PuzzleSolution([[$firstPiece->id],[$secondPiece->id]]);
+                return new PuzzleSolution([[$firstPiece->id],[$secondPiece->id]]);
             } elseif ($firstPiece->matchHorizontally($secondPiece)) {
-                $puzzleSolutionIndex = new PuzzleSolution([[$firstPiece->id,$secondPiece->id]]);
+                return new PuzzleSolution([[$firstPiece->id,$secondPiece->id]]);
             } elseif ($secondPiece->matchHorizontally($firstPiece)) {
-                $puzzleSolutionIndex = new PuzzleSolution([[$secondPiece->id,$firstPiece->id]]);
-            } else {
-                $puzzleSolutionIndex = new PuzzleSolution([[$secondPiece->id],[$firstPiece->id]]);
+                return new PuzzleSolution([[$secondPiece->id,$firstPiece->id]]);
             }
+            return new PuzzleSolution([[$secondPiece->id],[$firstPiece->id]]);
         }
 
         if (count($puzzlePieces) === 3) {
             $firstPiece = $puzzlePieces[0];
             $secondPiece = $puzzlePieces[1];
             $thirdPiece = $puzzlePieces[2];
-            $puzzleSolutionIndex = new PuzzleSolution([[$firstPiece->id,$secondPiece->id,$thirdPiece->id]]);
+            return new PuzzleSolution([[$firstPiece->id,$secondPiece->id,$thirdPiece->id]]);
         }
-        return $puzzleSolutionIndex;
+
+        return null;
     }
 }
