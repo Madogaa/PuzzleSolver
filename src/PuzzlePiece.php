cb03562ae798a7c7c82835695c9a2c90e01ff2db
mario.dominguez1 <mario.dominguez@fashiola.com>
2025-04-26 12:27:21 +0200

    Refactor: move method to PuzzlePiece class

--- src/PuzzlePiece.php
+++ src/PuzzlePiece.php
@@ -24,4 +24,14 @@ final readonly class PuzzlePiece
             (int) $pieceShapes[3]
         );
     }
+
+    public static function matchVertically(self $firstPiece, self $secondPiece): bool
+    {
+        return $firstPiece->bottom != 0 && $firstPiece->bottom == $secondPiece->top;
+    }
+
+    public static function matchHorizontally(self $firstPiece, self $secondPiece): bool
+    {
+        return $firstPiece->right != 0 && $firstPiece->right == $secondPiece->left;
+    }
 }
--- src/PuzzleSolver.php
+++ src/PuzzleSolver.php
@@ -30,11 +30,11 @@ final readonly class PuzzleSolver
         if (count($puzzlePieces) === 2) {
             $firstPiece = $puzzlePieces[0];
             $secondPiece = $puzzlePieces[1];
-            if (self::matchVertically($firstPiece, $secondPiece)) {
+            if (PuzzlePiece::matchVertically($firstPiece, $secondPiece)) {
                 $puzzleSolutionIndex = new PuzzleSolution([[1],[2]]);
-            } elseif (self::matchHorizontally($firstPiece, $secondPiece)) {
+            } elseif (PuzzlePiece::matchHorizontally($firstPiece, $secondPiece)) {
                 $puzzleSolutionIndex = new PuzzleSolution([[1,2]]);
-            } elseif (self::matchHorizontally($secondPiece, $firstPiece)) {
+            } elseif (PuzzlePiece::matchHorizontally($secondPiece, $firstPiece)) {
                 $puzzleSolutionIndex = new PuzzleSolution([[2,1]]);
             } else {
                 $puzzleSolutionIndex = new PuzzleSolution([[2],[1]]);
@@ -45,15 +45,5 @@ final readonly class PuzzleSolver
             $puzzleSolutionIndex = new PuzzleSolution([[1,2,3]]);
         }
         return $puzzleSolutionIndex;
-    }
-
-    private static function matchVertically(PuzzlePiece $firstPiece, PuzzlePiece $secondPiece): bool
-    {
-        return $firstPiece->bottom != 0 && $firstPiece->bottom == $secondPiece->top;
-    }
-
-    private static function matchHorizontally(PuzzlePiece $firstPiece, PuzzlePiece $secondPiece): bool
-    {
-        return $firstPiece->right != 0 && $firstPiece->right == $secondPiece->left;
     }
 }
