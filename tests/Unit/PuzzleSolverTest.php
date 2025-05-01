a16109c34d3c4c18bdce027aadbc3c2eab0ec588
mario.dominguez1 <mario.dominguez@fashiola.com>
2025-05-01 11:08:23 +0200

    Green test_it_solves2x3_puzzle

--- tests/Unit/PuzzleSolverTest.php
+++ tests/Unit/PuzzleSolverTest.php
@@ -117,14 +117,20 @@ class PuzzleSolverTest extends TestCase
         self::assertEquals($expectedPuzzleSolution, $puzzleSolution);
     }
 
-    public function test_it_solves3x3_puzzle(): void
+    public function test_it_solves2x3_puzzle(): void
     {
         $puzzleSolver = new PuzzleSolver();
-        $puzzlePieces = "3 3\n0 1 2 0\n0 3 4 1\n0 0 1 3\n2 2 3 0\n4 1 1 2\n1 0 3 1\n3 3 0 0\n1 1 0 3\n3 0 0 1";
+        $puzzlePieces = "2 3\n" .
+            "0 1 2 0\n" .   // ID 1
+            "0 3 4 1\n" .   // ID 2
+            "0 0 1 3\n" .   // ID 3
+            "2 2 3 0\n" .   // ID 4
+            "4 1 1 2\n" .   // ID 5
+            "1 0 3 1";      // ID 6
 
         $puzzleSolution = $puzzleSolver->solve($puzzlePieces);
 
-        $expectedPuzzleSolution = "1 2 3\n4 5 6\n7 8 9";
+        $expectedPuzzleSolution = "1 2 3\n4 5 6";
         self::assertEquals($expectedPuzzleSolution, $puzzleSolution);
     }
 
