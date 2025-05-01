502a5181ccceaf1636c3ebd03912d8934acda6a5
mario.dominguez1 <mario.dominguez@fashiola.com>
2025-05-01 11:31:49 +0200

    lint

--- src/Shared/AppLogger.php
+++ src/Shared/AppLogger.php
@@ -1,10 +1,12 @@
 <?php
 
+declare(strict_types=1);
+
 namespace App\Shared;
 
+use Monolog\Handler\StreamHandler;
 use Monolog\Level;
 use Monolog\Logger;
-use Monolog\Handler\StreamHandler;
 
 class AppLogger
 {
